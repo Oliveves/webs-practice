a88484d295c2e56cd515dbdaa6bcba5ab9feeae2
oliveves <oliveves@gmail.com>
2023-06-05 14:53:32 +0900

    최종 완료

--- 9-haeundae/script/script.js
+++ 9-haeundae/script/script.js
@@ -24,5 +24,25 @@ setInterval(function(){
     currentIndex = nextIndex;
 }, 3000)
 
+//콘텐츠 tab 메뉴
+
+$(".gallery").hide();
+
+$("h2").on("click", function(){
+    $(this).addClass("on").siblings("h2").removeClass("on");
+    $(".noticeGallery ul").hide(); $(".on+ul").show(); 
+});
+
+
+//팝업
+$("#pop").hide();
+
+$(".layerPopup").on("click", function(){
+    $("#pop").fadeIn();
+})
+
+$(".close").on("click", function(){
+    $("#pop").fadeOut();
+})
 
 });
