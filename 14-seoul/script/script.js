fabd68690c757adacba94641268bb0d8d615727b
oliveves <oliveves@gmail.com>
2023-06-15 14:29:55 +0900

    14. 완료

---
+++ 14-seoul/script/script.js
@@ -0,0 +1,42 @@
+$(window).on("load", function(){
+    //메뉴
+    $(".submenu").hide();
+
+    $("nav > ul > li").hover(function(){
+        $(this).find($("ul")).stop().slideDown(500);
+    }, function(){
+        $(this).find($("ul")).stop().slideUp(500);
+    });
+
+
+    //반응형 - 좌우슬라이드
+
+    let currentInex = 0;
+
+    $(".slideList").append($(".slideImg").first().clone(true));
+    
+    setInterval(function(){
+        currentInex ++;
+        $(".slideList").animate({marginLeft : -currentInex * $(".slideList").width()}, 600);
+
+        if(currentInex == 3){
+            setTimeout(function(){
+                $(".slideList").animate({marginLeft : 0}, 0);
+                currentInex = 0;
+            }, 700)
+        }
+    }, 3000)
+
+    //팝업
+    $("#pop").hide();
+
+    $(".notice ul li a").first().addClass("layerPopup");
+    $(".layerPopup").on("click", function(){
+        $("#pop").fadeIn();
+    })
+    $(".close").on("click", function(){
+        $("#pop").fadeOut();
+    })
+
+
+})
